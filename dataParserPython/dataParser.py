48db62eaa871504d7a5c0b42a1f2af39439ed771
mchristo <mchristo28@gmail.com>
2019-07-26 10:06:43 -0700

    Did stuff

--- dataParserPython/dataParser.py
+++ dataParserPython/dataParser.py
@@ -11,21 +11,69 @@ class ReadAlloftheDataAndStuffALSOProbablyDoSomethingWithIt:
         self.dataFile = None
 
     def readData(self):
-        print('You created an object! FileName given: ' + self.fileName)
-        self.dataFile = open(self.fileName, 'rb')
+        #print('You created an object! FileName given: ' + self.fileName)
+        try: self.dataFile = open(self.fileName, 'rb')
+        except FileNotFoundError:
+            pass    
         if (self.dataFile):
             print('Opened Successfully!!!')
+            self.parseHeader()
             self.parseData()
             self.dataFile.close()
+        else:
+            print('Could not open file: ' + self.fileName)
 
     def parseHeader(self):
-        pass #TODO parse header 
+        print('****************************************')
+        date = self.dataFile.read(10)
+        print('Date:', end =" ")
+        print(date.decode("utf-8"))
+        print('****************************************')
+        print(' ')
+        # Parse floats
+        print('****************************************')
+        print('**************** HEADER ****************')
+        print('****************************************')
+        chirpBw = self.dataFile.read(4) #reads float
+        chirpBw = struct.unpack('f', chirpBw)[0]
+        print('Bandwidth: ' + str(chirpBw) + '%')
+        chirpCf = self.dataFile.read(4)
+        chirpCf = struct.unpack('f', chirpCf)[0]
+        print('Center Frequency: ' + str(chirpCf / 1e6) + ' MHz')
+        chirpTL = self.dataFile.read(4)
+        chirpTL = struct.unpack('f', chirpTL)[0]
+        print('Chirp Time Length: ' + str(round(chirpTL * 1e6, 1)) + ' us')
+        chirpAmp = self.dataFile.read(4)
+        chirpAmp = struct.unpack('f', chirpAmp)[0]
+        print('Amplitude: ' + str(round(chirpAmp, 3)))
+        prf = self.dataFile.read(4)
+        prf = struct.unpack('f', prf)[0]
+        print('Pulse Repetition Frequency: ' + str(prf / 1e3) + ' KHz')
+        receiveTL = self.dataFile.read(4)
+        receiveTL = struct.unpack('f', receiveTL)[0]
+        print('Trace Time Length: ' + str(round(receiveTL * 1e6, 1)) + ' us')
+
+        # Parse ints
+        txSampFreq = self.dataFile.read(4)
+        txSampFreq = struct.unpack('i', txSampFreq)[0]
+        print('Transmit Sampling Frequency: ' + str(txSampFreq / 1e6) + ' MHz')
+        rxSampFreq = self.dataFile.read(4)
+        rxSampFreq = struct.unpack('i', rxSampFreq)[0]
+        print('Receiver Sampling Frequency: ' + str(rxSampFreq / 1e6) + ' MHz')
+        stackSize = self.dataFile.read(4)
+        stackSize = struct.unpack('i', stackSize)[0]
+        print('Stack Size: ' + str(stackSize))
+
 
     def parseData(self):
-        gpsDate = self.dataFile.read(15)
-        print('GPS Time:'),
+        print('\n')
+        print('****************************************')
+        print('****************  DATA  ****************')
+        print('****************************************')
+        gpsDate = self.dataFile.read(13)
+        print('GPS Time:', end =" ")
         print(gpsDate.decode("utf-8"))
-        lat = self.dataFile.read(4) #NEED TO UNPACK
+        lat = self.dataFile.read(4)
         lat = struct.unpack('f', lat)[0]
         print('Latitude:'),
         print(lat)
---
+++ dataParserPython/vervetRunPy.sh
@@ -0,0 +1,1 @@
+python dataParser.py -f /home/vervet/Desktop/msc_x310/usrp_samples.dat
